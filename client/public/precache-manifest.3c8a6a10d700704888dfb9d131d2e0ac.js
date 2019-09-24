3a6097f5557a710b56c9c16a8520db52cc313596
Cris Smith <cris.smith.fa@gmail.com>
2019-09-24 01:46:43 -0500

    moved build files to public folder

---
+++ client/public/precache-manifest.3c8a6a10d700704888dfb9d131d2e0ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31821847154aa56a5371b03b316301ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05460c9cbc2796b0d07f",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b3aa91d4fe3016bb6d5a",
+    "url": "/static/css/main.0cb45a0d.chunk.css"
+  },
+  {
+    "revision": "05460c9cbc2796b0d07f",
+    "url": "/static/js/2.44f42ce6.chunk.js"
+  },
+  {
+    "revision": "b3aa91d4fe3016bb6d5a",
+    "url": "/static/js/main.6be71d2d.chunk.js"
+  },
+  {
+    "revision": "2055ac218fe3f7ad47ca",
+    "url": "/static/js/runtime-main.a7ca617d.js"
+  }
+]);
